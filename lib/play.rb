1283ac53c91cb3ddfc184df4ef3ae67fc12837ec
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-07-23 19:41:43 +0100

    feat: limited guesses

--- lib/play.rb
+++ lib/play.rb
@@ -2,6 +2,7 @@
 class Hangman
   def initialize
     @word = words.sample
+    @lives = 5
   end
 
   def words
@@ -25,26 +26,39 @@ class Hangman
   end
   
   def hint
-    puts "Word size is #{@word.first.length} characters"
     puts "Your clue is: #{@word.last}"
+    puts "Word size is #{@word.first.length} characters"
+  end
+
+
+  def make_guess
+    if @lives > 0 
+      puts "enter a letter"
+      guess = gets.chomp
+      # guess = "A".downcase
+
+      good_guess = @word.first.include? guess
+
+      if good_guess
+        puts "good guess"
+        make_guess
+      else
+        @lives -= 1
+        puts "wrong you have #{@lives} lives left. try again!"
+        make_guess
+      end
+
+    else 
+      puts "game over"
+    end
   end
 
   def begin 
     puts "Game has started"
-    puts "enter a letter"
     puts hint
     puts teaser
+    puts make_guess
   end
-
-  def guess
-    # guess = gets.chomp.downcase
-    guess = "A".downcase
-    puts "#{guess}"
-    
-
-  end
-
-
 
 
 end
