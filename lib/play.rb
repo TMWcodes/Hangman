eadcccafa8050da3a9ada5624b51ab982c87c93a
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-07-23 15:53:59 +0100

    feat: add clues to game start

--- lib/play.rb
+++ lib/play.rb
@@ -6,21 +6,22 @@ class Hangman
 
   def words
     [
-      ['smile'],
-      ['robot'],
-      ['pedal']
-      # ['candle'],
-      # ['tissue']
+      ['smile', "a friendly face"],
+      ['robot', "a moving metal object"],
+      ['pedal', "found on a bike"],
+      ['candle', "used to illuminate darkness"],
+      ['tissue', "used to wipe surfaces"]
       
     ]
     
   end
- 
   def begin 
     puts "game has started"
+    puts "your clue is: #{@word.last}"
     "word size is #{@word.first.length} characters"
   end
 end
 
 hang = Hangman.new
+# puts hang.words.sample
 puts hang.begin
--- spec/play_spec.rb
+++ spec/play_spec.rb
@@ -2,15 +2,14 @@ require 'play'
 
 describe 'play' do
     
-    it 'contains a selection of words' do
+    it 'contains a selection of words and clues' do
         hang = Hangman.new
        
-        expect(hang.words).to include(['smile'])
+        expect(hang.words).to include(["smile", "a friendly face"])
     end
-
+    
     it 'begins game' do
         hang = Hangman.new
-        words = 
         expect(hang.begin).to eq("word size is 5 characters")
     end
 end
